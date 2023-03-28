e6bcf178a20a41cbf733eafc6f17f5e6f57fa556
stasevds <staseffds@gmail.com>
2023-03-28 21:31:41 +0300

    update file

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function sum(number){
+/*function sum(number){
   let result = number;
 
   function f(nextNumber){
@@ -14,4 +14,35 @@ function sum(number){
   return f;
 }
 console.log(sum(1)(2)(3));
-//alert(sum(1)(2)(3))
+alert(sum(1)(2)(3))*/
+
+
+
+//const testElement = document.querySelector('#test')
+//const testElement = document.getElementsByClassName('test');
+//const testElement2 = document.getElementById('#test')
+
+//const third = document.body.querySelector("[data-property='3']")
+//testElement2.dataset.property = '22';
+
+//console.log(third);
+//console.log(document.body.children)
+//console.log(document.body.style.background ='yellow')
+//console.log(testElement.dataset.property)
+//console.log(testElement2)
+
+//console.log(document.body.querySelectorAll('.test').children);
+
+const elements = document.body.querySelectorAll(".test");
+const first = elements[0].dataset.property;
+elements.forEach((el,index, array)=>{
+  console.log(el)
+  const nextSibling = el.nextElementSibling;
+  if(nextSibling) {
+    el.dataset.property= nextSibling.dataset.property
+  }
+  else {el.dataset.property = array[0].dataset.property
+}})
+
+   
+

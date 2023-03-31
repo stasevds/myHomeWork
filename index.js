f628e789609dfcf81b4b8ef4629f07ad018e6c89
stasevds <staseffds@gmail.com>
2023-03-31 22:20:36 +0300

    HW_two

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
-function sum(number){
-  let result = number;
-
-  function f(nextNumber){
-    result += nextNumber;
-    //console.log(result);
-    return f;
-  }
-  f.toString = function() {
-    console.log(result);
-    return result;
-  };
+let array = [];
+let rainbow_div = document.querySelector(".rainbow");
+for(let i=0; i<rainbow_div.children.length;i++){
+  //console.log(rainbow_div.children[i])
+  rainbow_div.children[i].style.background = rainbow_div.children[i].className;
+  array.push(rainbow_div.children[i]);
   
-  return f;
 }
-console.log(sum(1)(2)(3));
-//alert(sum(1)(2)(3))
+//console.log(rainbow_div)
+for(let i = array.length-1; i>0; i--){
+  array[i-1].appendChild(array[i]);
+}

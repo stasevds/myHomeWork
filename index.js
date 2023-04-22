6c8a628b30b6a66bf323650133eb58c0a19c9746
stasevds <staseffds@gmail.com>
2023-04-22 12:10:27 +0300

    new commit

---
+++ index.js
@@ -0,0 +1,15 @@
+/*const string = "Lorem ipsum dolor sit amet, lorem consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." 
+
+//const regex = new RegExp("Lorem","gmi");
+// или так
+const regex = /Lorem/gi;
+
+console.log(string.match(regex))
+console.log(regex.test(string))*/
+
+//import{name, surname} from './test.js'
+//import name from './test.js'
+
+import* as obj from './test.js'
+
+console.log(obj.name)
---
+++ test.js
@@ -0,0 +1,9 @@
+const name = "vasya";
+export default 'hello';
+
+const surname = "pupkin";
+
+export {
+    name,
+    surname
+}

4b53888a23993cbbb2e930fb84d2d298357d60c7
stasevds <staseffds@gmail.com>
2023-05-21 22:45:43 +0300

    add new commit with wabpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,55 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+//const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
- mode: 'development',   
- entry:'/src/index.js',
- output:{
-    filename: "bundle.js",
-    path:'/dist'
- },
- resolve:{
+   mode: 'development',   
+   entry:'/src/index.js',
+   output:{
+     filename: "bundle.js",
+     path:'/dist'
+   },
+   devServer:{
+      static:{
+         directory: path.join(__dirname, 'public'),
+      },
+      compress: true,
+      port: 9000,
+      hot: true, 
+   },
+   watchOptions:{
+      aggregateTimeout:100,
+      ignored:['node_modules/**'],
+   },
+   resolve:{
     extensions: ['.js']
- },
+   },
+   module:{
+      rules:[
+         {
+            test:/\.css$/i,
+            use: [
+               "style-loader",
+               //MiniCssExtractPlugin.loader,
+               "css-loader"
+            ], 
+         },
+         {
+            test:/\.(png|jpe?g|gif)$/i,
+            use:[
+               {
+                  loader: 'file-loader',
+               }, 
+            ]
+         }
+      ]
+   },
+   plugins: [
 
+      new HtmlWebpackPlugin({
+       filename: "index.html",
+       // favicon: "/public/favicon.ico" 
+       template: path.join(__dirname, '/public/index.html'), 
+      })
+   ]
 }
